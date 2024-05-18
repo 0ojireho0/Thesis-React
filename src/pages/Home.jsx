0df31e66ed878ae68ec7fa38cb2afe60190f44e4
0ojireho0 <jeremiahquintano17@gmail.com>
2024-05-18 12:27:02 +0800

    change in homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,9 +9,15 @@ import monitor from "../assets/Monitor.png"
 
 
 import { Button } from '@material-tailwind/react'
-import { NavLink } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 const Home = () => {
+
+  const navigate = useNavigate()
+
+  const handleToRegister = () =>{
+    navigate('/register')
+  }
 
     return (
         <div className='w-full h-screen bg-gradient-to-br from-[#5A7D94] to-[#1C272E]'>
@@ -24,7 +30,7 @@ const Home = () => {
             </div>
             <div className='absolute top-56 ml-10'>
                 <h1 className=' poppins-bold w-80 ml-[35rem]  text-lg'><span className='text-white'>Transform Your Workout with AI:</span> <span className='text-[#94C9D8] '>Perfect Your Posture</span><span className='text-white'>, </span><span className='text-[#F7813C]'>Maximize Your Results</span></h1>
-                <Button className='ml-[40rem] w-22 mt-3 rounded-full' color='white'><NavLink to="/register">Register</NavLink></Button>
+                <Button className='ml-[40rem] w-22 mt-3 rounded-full' color='white' onClick={handleToRegister}>REGISTER</Button>
             </div>
             <div className='w-[50rem] mt-12'>
                 <img src={monitor} alt="" className='' />
